ac3b2fd2c5a2eef6d4c18c802140807a3f90e954
Andy (Yicheng) Li <101478069+AndyLi1998@users.noreply.github.com>
2022-03-15 18:18:36 -0400

    Add files via upload
    Sensor Testing

---
+++ GY521_and_RTS_I2C_test.ino
@@ -0,0 +1,204 @@
+
+#include "Wire.h" 
+const int MPU_ADDR = 0x69; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
+const int DS3231_I2C_ADDRESS = 0x68;
+
+int16_t accelerometer_x, accelerometer_y, accelerometer_z; // variables for accelerometer raw data
+int16_t gyro_x, gyro_y, gyro_z; // variables for gyro raw data
+int16_t temperature; // variables for temperature data
+
+char tmp_str[7]; // temporary variable used in convert function
+
+char* convert_int16_to_str(int16_t i) { // converts int16 to string. Moreover, resulting strings will have the same length in the debug monitor.
+  sprintf(tmp_str, "%6d", i);
+  return tmp_str;
+}
+
+byte seconds, minutes, hours, day, date, month, year;
+char weekDay[4];
+byte tMSB, tLSB;
+float my_temp;
+char my_array[100];            // Character array for printing something.
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  Wire.beginTransmission(MPU_ADDR); // Begins a transmission to the I2C slave (GY-521 board)
+  Wire.write(0x6B); // PWR_MGMT_1 register
+  Wire.write(0); // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+}
+void loop() {
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
+  Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
+  Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
+  
+  // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
+  accelerometer_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
+  accelerometer_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
+  accelerometer_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
+  temperature = Wire.read()<<8 | Wire.read(); // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
+  gyro_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
+  gyro_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
+  gyro_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+  
+  // print out data
+  Serial.print("aX = "); Serial.print(convert_int16_to_str(accelerometer_x));
+  Serial.print(" | aY = "); Serial.print(convert_int16_to_str(accelerometer_y));
+  Serial.print(" | aZ = "); Serial.print(convert_int16_to_str(accelerometer_z));
+  // the following equation was taken from the documentation [MPU-6000/MPU-6050 Register Map and Description, p.30]
+  Serial.print(" | tmp = "); Serial.print(temperature/340.00+36.53);
+  Serial.print(" | gX = "); Serial.print(convert_int16_to_str(gyro_x));
+  Serial.print(" | gY = "); Serial.print(convert_int16_to_str(gyro_y));
+  Serial.print(" | gZ = "); Serial.print(convert_int16_to_str(gyro_z));
+  Serial.println();
+  
+  // delay
+  delay(1000);
+  watchConsole(); 
+  get3231Date();
+  Serial.print(weekDay); 
+  Serial.print(", "); 
+  Serial.print(date, DEC); 
+  Serial.print("/"); 
+  Serial.print(month, DEC);
+  Serial.print("/"); 
+  Serial.print(year, DEC); 
+  Serial.print(" - ");
+  Serial.print(hours, DEC);
+  Serial.print(":"); 
+  Serial.print(minutes, DEC);
+  Serial.print(":"); 
+  Serial.print(seconds, DEC);
+
+  my_temp = (float)get3231Temp();
+ 
+  Serial.print(" - Temp: "); 
+  Serial.println(my_temp);
+  
+
+  
+sprintf(my_array, "Time: %d:%d:%d", hours, minutes, seconds);
+}
+
+// Convert normal decimal numbers to binary coded decimal
+byte decToBcd(byte val)
+{
+  return ( (val/10*16) + (val%10) );
+}
+
+void watchConsole()
+{
+  if (Serial.available()) {      // Look for char in serial queue and process if found
+    if (Serial.read() == 84) {      //If command = "T" Set Date
+      set3231Date();
+      get3231Date();
+      Serial.println(" ");
+    }
+  }
+}
+
+void set3231Date()
+{
+//T(sec)(min)(hour)(dayOfWeek)(dayOfMonth)(month)(year)
+//T(00-59)(00-59)(00-23)(1-7)(01-31)(01-12)(00-99)
+//Example: 02-Feb-09 @ 19:57:11 for the 3rd day of the week -> T1157193020209
+// T1124154091014
+  seconds = (byte) ((Serial.read() - 48) * 10 + (Serial.read() - 48)); // Use of (byte) type casting and ascii math to achieve result.  
+  minutes = (byte) ((Serial.read() - 48) *10 +  (Serial.read() - 48));
+  hours   = (byte) ((Serial.read() - 48) *10 +  (Serial.read() - 48));
+  day     = (byte) (Serial.read() - 48);
+  date    = (byte) ((Serial.read() - 48) *10 +  (Serial.read() - 48));
+  month   = (byte) ((Serial.read() - 48) *10 +  (Serial.read() - 48));
+  year    = (byte) ((Serial.read() - 48) *10 +  (Serial.read() - 48));
+  Wire.beginTransmission(DS3231_I2C_ADDRESS);
+  Wire.write(0x00);
+  Wire.write(decToBcd(seconds));
+  Wire.write(decToBcd(minutes));
+  Wire.write(decToBcd(hours));
+  Wire.write(decToBcd(day));
+  Wire.write(decToBcd(date));
+  Wire.write(decToBcd(month));
+  Wire.write(decToBcd(year));
+  Wire.endTransmission();
+}
+
+
+void get3231Date()
+{
+  // send request to receive data starting at register 0
+  Wire.beginTransmission(DS3231_I2C_ADDRESS); // 104 is DS3231 device address
+  Wire.write(0x00); // start at register 0
+  Wire.endTransmission();
+  Wire.requestFrom(DS3231_I2C_ADDRESS, 7); // request seven bytes
+
+  if(Wire.available()) {
+    seconds = Wire.read(); // get seconds
+    minutes = Wire.read(); // get minutes
+    hours   = Wire.read();   // get hours
+    day     = Wire.read();
+    date    = Wire.read();
+    month   = Wire.read(); //temp month
+    year    = Wire.read();
+       
+    seconds = (((seconds & B11110000)>>4)*10 + (seconds & B00001111)); // convert BCD to decimal
+    minutes = (((minutes & B11110000)>>4)*10 + (minutes & B00001111)); // convert BCD to decimal
+    hours   = (((hours & B00110000)>>4)*10 + (hours & B00001111)); // convert BCD to decimal (assume 24 hour mode)
+    day     = (day & B00000111); // 1-7
+    date    = (((date & B00110000)>>4)*10 + (date & B00001111)); // 1-31
+    month   = (((month & B00010000)>>4)*10 + (month & B00001111)); //msb7 is century overflow
+    year    = (((year & B11110000)>>4)*10 + (year & B00001111));
+  }
+  else {
+    //oh noes, no data!
+  }
+ 
+  switch (day) {
+    case 1:
+      strcpy(weekDay, "Sun");
+      break;
+    case 2:
+      strcpy(weekDay, "Mon");
+      break;
+    case 3:
+      strcpy(weekDay, "Tue");
+      break;
+    case 4:
+      strcpy(weekDay, "Wed");
+      break;
+    case 5:
+      strcpy(weekDay, "Thu");
+      break;
+    case 6:
+      strcpy(weekDay, "Fri");
+      break;
+    case 7:
+      strcpy(weekDay, "Sat");
+      break;
+  }
+}
+
+float get3231Temp()
+{
+  float temp3231;
+  
+  //temp registers (11h-12h) get updated automatically every 64s
+  Wire.beginTransmission(DS3231_I2C_ADDRESS);
+  Wire.write(0x11);
+  Wire.endTransmission();
+  Wire.requestFrom(DS3231_I2C_ADDRESS, 2);
+ 
+  if(Wire.available()) {
+    tMSB = Wire.read(); //2's complement int portion
+    tLSB = Wire.read(); //fraction portion
+   
+    temp3231 = (tMSB & B01111111); //do 2's math on Tmsb
+    temp3231 += ( (tLSB >> 6) * 0.25 ); //only care about bits 7 & 8
+  }
+  else {
+    //oh noes, no data!
+  }
+   
+  return temp3231;
+}
---
+++ PH4502C.ino
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+int pHSense = A0;
+int samples = 10;
+float adc_resolution = 1024.0;
+float calibration_value = 21.34 - 0.7;
+float ph_act;
+void setup()
+{
+  Serial.begin(9600);
+  delay(100);
+  Serial.println("cimpleo pH Sense");
+}
+
+float ph (float voltage) {
+  return 7 + ((2.5 - voltage) / 0.18);
+}
+
+void loop () {
+  int measurings=0;
+  for (int i = 0; i < samples; i++)
+  {
+    measurings += analogRead(pHSense);
+    delay(10);
+  }
+  
+    float voltage = 5 / adc_resolution * measurings/samples;
+    ph_act = -5.70 * voltage + calibration_value;
+    Serial.print("pH= ");
+    Serial.println(ph_act);
+    delay(1000);
+}
